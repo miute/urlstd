077a753ee851db48171d091229463c7d0a030526
Miute <miute.dev@gmail.com>
2022-11-22 01:02:58 +0900

    Update docs to v2021.10.25.post2

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,7 +1,7 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.10.25.post1',
-    LANGUAGE: 'None',
+    VERSION: '2021.10.25.post2',
+    LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
